350eecb3c7507b070de2d3417bfe8c703a08c7bc
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-03-16 15:30:40 +0700

    feat: user action constants

---
+++ src/constants/actions.js
@@ -0,0 +1,15 @@
+class KUserActions {
+  static PENDING_SIGN_IN = "SIGN_IN";
+  static SIGN_IN_SUCCESS = "SIGN_IN_SUCCESS";
+  static SIGN_IN_FAILED = "SIGN_IN_FAILED";
+
+  static PENDING_SIGN_UP = "SIGN_UP";
+  static SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
+  static SIGN_UP_FAILED = "SIGN_UP_FAILED";
+
+  static PENDING_SIGN_OUT = "SIGN_OUT";
+  static SIGN_OUT_SUCCESS = "SIGN_OUT_SUCCESS";
+  static SIGN_OUT_FAILED = "SIGN_OUT_FAILED";
+}
+
+export { KUserActions };
