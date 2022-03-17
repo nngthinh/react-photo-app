af2cd524b9d05d92280db398d30d4ca1723fc732
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-03-17 16:35:35 +0700

    feat: category repository

--- src/repositories/category.js
+++ src/repositories/category.js
@@ -1,3 +1,27 @@
+import { baseUrl } from "constants/apiUrl";
+const { RestService } = require("utils/services/rest");
+
 class Category {
-  static getcategoriesList() {}
+  static async createCategory(name, description, image_url) {
+    const url = `${baseUrl}/categories`;
+    const body = {
+      name: name,
+      description: description,
+      imageUrl: url,
+    };
+    
+    try {
+      const result = await RestService.postWithToken(url, body);
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async viewCateogries(offset, limit) {
+    const url = `${baseUrl}/categories?offset=${offset}&limit=${limit}`;
+    try {
+      const result = await 
+    }
+  }
 }
