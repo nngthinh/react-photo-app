0a6fbb297ca7b6dfba0fd42b8e15974f41c0dbba
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-03-18 00:46:58 +0700

    update: full user action

--- src/actions/user.js
+++ src/actions/user.js
@@ -10,9 +10,14 @@ const signUpAction = (name, email, password) => ({
   pendingAction: async () => await UserRepository.signUp(name, email, password),
 });
 
+const signOutAction = () => ({
+  pendingActionType: "SIGN_OUT",
+  pendingAction: async () => await UserRepository.signOut(),
+});
+
 const getUserInfoAction = () => ({
   pendingActionType: "GET_USER_INFO",
   pendingAction: async () => await UserRepository.getUserInfo(),
 });
 
-export { signInAction, signUpAction, getUserInfoAction };
+export { signInAction, signUpAction, signOutAction, getUserInfoAction };
