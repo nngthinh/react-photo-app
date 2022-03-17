0b9084fce9901222d212c68f81f0d3ca7254c023
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-03-18 01:03:38 +0700

    feat: nav bar

--- src/components/Common/NavBar/index.js
+++ src/components/Common/NavBar/index.js
@@ -1,18 +1,26 @@
+import { signOutAction } from "actions/user";
+import { useDispatch, useSelector } from "react-redux";
 import "./Navbar.css";
 
 const Navbar = () => {
-  return <NavbarView />;
+  const userInfo = useSelector((state) => state.user.info);
+  const dispatch = useDispatch();
+  const dispatchSignOut = () => dispatch(signOutAction());
+  return <NavbarView userInfo={userInfo} onSignOut={dispatchSignOut} />;
 };
 
-const NavbarView = () => {
+const NavbarView = ({ userInfo, onSignOut }) => {
+  const isLoggedIn = userInfo ? true : false;
   return (
     <div className="navbar">
       <div className="navbarWrapper">
         <div className="left">
-          <div>Welcome to Photo app</div>
+          <div>
+            {isLoggedIn ? `Hi ${userInfo.name}!` : "Welcome to Photo app!"}
+          </div>
         </div>
         <div className="right">
-          <button>Sign In</button>
+          {isLoggedIn ? <button>Sign Out</button> : <button>Sign In</button>}
         </div>
       </div>
     </div>
