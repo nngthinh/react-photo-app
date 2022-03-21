e5953817fb0c3f0cc7ae34f54ec221c3ee5d27f0
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-03-21 09:47:30 +0700

    feat: common validation

---
+++ src/utils/validation/common.js
@@ -0,0 +1,20 @@
+const isValidUrl = (url) => {
+  try {
+    new URL(url);
+    return true;
+  } catch (_) {
+    return false;
+  }
+};
+
+const isValidPassword = (password) =>
+  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/.test(password);
+
+const isValidEmail = (email) =>
+  String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+
+export { isValidEmail, isValidPassword, isValidUrl };
