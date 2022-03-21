f429290a4d40e3358e5c368f19e881f21d0add35
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-03-21 11:23:40 +0700

    update: category action

--- src/actions/categories.js
+++ src/actions/categories.js
@@ -14,7 +14,7 @@ const createCategoriesListAction = (name, description, imageUrl) => ({
 
 const viewCategoryDetailAction = (categoryId) => ({
   pendingActionType: KCategoriesAction.PENDING_VIEW_CATEGORY,
-  pendingAction: async () => CategoriesRepository.createCategory(categoryId),
+  pendingAction: async () => CategoriesRepository.viewCategory(categoryId),
 });
 
 const updateCategoryDetailAction = (
@@ -22,7 +22,10 @@ const updateCategoryDetailAction = (
   name,
   description,
   imageUrl
-) => ({});
+) => ({
+    pendingActionType: KCategoriesAction.PENDING_UPDATE_CATEGORY,
+    pendingAction: async () => CategoriesRepository.updateCategory(categoryId, name, description, imageUrl);
+});
 
 export {
   viewCategoriesListAction,
