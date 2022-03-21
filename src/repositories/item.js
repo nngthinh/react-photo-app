4bad110c4118e06de33404d27b6721248d47ce25
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-03-21 11:07:02 +0700

    update: category repository

---
+++ src/repositories/item.js
@@ -0,0 +1,59 @@
+import { baseUrl } from "constants/apiUrl";
+const { RestService } = require("utils/services/rest");
+
+class ItemsRepository {
+  static async viewItems(categoryId, offset, limit) {
+    const url = `${baseUrl}/categories/${categoryId}/items?offset=${offset}&limit=${limit}`;
+    try {
+      const result = await RestService.getWithToken(url);
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async createItem(categoryId, description, imageUrl) {
+    const url = `${baseUrl}/categories/${categoryId}/items`;
+    const body = { description, imageUrl };
+
+    try {
+      const result = await RestService.postWithToken(url, body);
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async viewItem(categoryId, itemId) {
+    const url = `${baseUrl}/categories/${categoryId}/items/${itemId}`;
+    try {
+      const result = await RestService.getWithToken(url);
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async updateItem(categoryId, itemId, description, imageUrl) {
+    const url = `${baseUrl}/categories/${categoryId}/items/${itemId}`;
+    const body = { description, imageUrl };
+    try {
+      const result = await RestService.putWithToken(url, body);
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async deleteItem(categoryId, itemId) {
+    const url = `${baseUrl}/categories/${categoryId}/items/${itemId}`;
+    try {
+      const result = await RestService.deleteWithToken(url);
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default ItemsRepository;
