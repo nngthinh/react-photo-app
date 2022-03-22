92e3449962232815f6c45398d635e7c8b2d1128e
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-03-22 16:04:36 +0700

    update: item reducer

--- src/components/Home/CategoryDetail/index.js
+++ src/components/Home/CategoryDetail/index.js
@@ -1,11 +1,49 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useParams, useSearchParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { viewCategoryAction } from "actions/categories";
+import { viewItemsListAction } from "actions/items";
+import { PaginationItem } from "components/Common/Items";
+
 const CategoryDetail = () => {
-  return <CategoryDetailView></CategoryDetailView>;
+  const { categoryId } = useParams();
+  const categoryDetail = useSelector((state) => state.categories.detail);
+  const itemsList = useSelector((state) => state.items.list);
+  const itemsPaginationTotal = useSelector(
+    (state) => state.items.pagination.total
+  );
+  const dispatch = useDispatch();
+
+  // Items pagination
+  const [searchParams, setSearchParams] = useSearchParams();
+  let page = parseInt(searchParams.get("page") ?? 0);
+
+  useEffect(() => {
+    // get category detail
+    // dispatch(viewCategoryAction());
+    // and get items list
+    // dispatch(viewItemsListAction());
+  }, [dispatch, categoryId]);
+
+  return (
+    <CategoryDetailView
+      categoryDetail={categoryDetail}
+      itemsList={itemsList}
+      itemsPagination={{ maxIndex: itemsPaginationTotal ?? 0 }}
+    ></CategoryDetailView>
+  );
 };
 
-const CategoryDetailView = () => {
+const CategoryDetailView = ({
+  categoryDetail,
+  itemsPagination,
+  itemsList = [],
+} = {}) => {
   return (
     <>
       <div>Category Detail</div>
+      <PaginationItem {...itemsPagination}></PaginationItem>
     </>
   );
 };
--- src/reducers/items.js
+++ src/reducers/items.js
@@ -1,26 +1,31 @@
 import { KItemsAction } from "constants/actions";
-import { limitItems } from "constants/pagination";
 
 const initialState = {
-  pagination: {
-    limit: limitItems,
-    currentPage: 0,
-  },
-  data: {},
+  pagination: {},
+  list: {},
+  detail: {},
 };
 
 const itemsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case KItemsAction.VIEW_ITEMS_SUCCESS:
-      return { ...state };
+    case KItemsAction.VIEW_ITEMS_SUCCESS: {
+      const itemInfo = action.data;
+      return {
+        ...state,
+        pagination: { total: itemInfo.totalItems },
+        list: itemInfo.items,
+      };
+    }
     case KItemsAction.VIEW_ITEMS_FAILED:
       return { ...state };
     case KItemsAction.CREATE_ITEM_SUCCESS:
       return { ...state };
     case KItemsAction.CREATE_ITEM_FAILED:
       return { ...state };
-    case KItemsAction.VIEW_ITEM_SUCCESS:
-      return { ...state };
+    case KItemsAction.VIEW_ITEM_SUCCESS: {
+      const itemInfo = action.data;
+      return { ...state, detail: itemInfo };
+    }
     case KItemsAction.VIEW_ITEM_FAILED:
       return { ...state };
     case KItemsAction.UPDATE_ITEM_SUCCESS:
