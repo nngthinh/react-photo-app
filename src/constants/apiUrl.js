e59882e468bb3a2e61e7a80f499c2bccfce22914
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-03-27 22:07:59 +0700

    fix (temp): base api based on node env

--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -1,4 +1,6 @@
-export const baseUrl =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:8080"
-    : "https://somewebsite.com";
+// export const baseUrl =
+//   process.env.NODE_ENV === "development"
+//     ? "http://localhost:8080"
+//     : "https://somewebsite.com";
+
+export const baseUrl = "http://localhost:8080";
