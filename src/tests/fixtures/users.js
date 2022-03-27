b8bec2649d558a7244634faa064cec9b51e4d951
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-03-27 17:36:04 +0700

    update: change fixtures format from json to js

---
+++ src/tests/fixtures/users.js
@@ -0,0 +1,19 @@
+const user = {
+  info: {
+    "admin@mail.com": {
+      id: 1,
+      name: "Admin",
+      password: "123abC#",
+    },
+    "otherAdmin@mail.com": {
+      id: 2,
+      name: "Other Admin",
+      password: "987cbA&",
+    },
+  },
+  token: {
+    tokenAdmin: "admin@mail.com",
+    tokenOtherAdmin: "otehrAdmin@mail.com",
+  },
+};
+export default user;
