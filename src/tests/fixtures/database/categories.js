cc6dae995ed506c7dc5772d9e9b259bed7d33b13
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-03-28 11:32:40 +0700

    fix (imp): list format

--- src/tests/fixtures/database/categories.js
+++ src/tests/fixtures/database/categories.js
@@ -1,56 +1,56 @@
 const categoriesData = {
-  items: {
-    1: {
+  items: [
+    {
       name: "category 1",
       description: "description 1",
       imageUrl: "https://picsum.photos/200",
     },
-    2: {
+    {
       name: "category 2",
       description: "description 2",
       imageUrl: "https://picsum.photos/200",
     },
-    3: {
+    {
       name: "category 3",
       description: "description 3",
       imageUrl: "https://picsum.photos/200",
     },
-    4: {
+    {
       name: "category 4",
       description: "description 4",
       imageUrl: "https://picsum.photos/200",
     },
-    5: {
+    {
       name: "category 5",
       description: "description 5",
       imageUrl: "https://picsum.photos/200",
     },
-    6: {
+    {
       name: "category 6",
       description: "description 6",
       imageUrl: "https://picsum.photos/200",
     },
-    7: {
+    {
       name: "category 7",
       description: "description 7",
       imageUrl: "https://picsum.photos/200",
     },
-    8: {
+    {
       name: "category 8",
       description: "description 8",
       imageUrl: "https://picsum.photos/200",
     },
-    9: {
+    {
       name: "category 9",
       description: "description 9",
       imageUrl: "https://picsum.photos/200",
     },
-    10: {
+    {
       name: "category 10",
       description: "description 10",
       imageUrl: "https://picsum.photos/200",
     },
-  },
+  ],
   totalItems: 10,
 };
 
--- src/tests/fixtures/database/items.js
+++ src/tests/fixtures/database/items.js
@@ -1,114 +1,114 @@
 const itemsData = {
-  items: {
-    1: {
+  items: [
+    {
       description: "item description 1",
       image_url: "https://picsum.photos/200",
       author: 1,
       category_id: 1,
     },
-    2: {
+    {
       description: "item description 2",
       image_url: "https://picsum.photos/200",
       author: 1,
       category_id: 1,
     },
-    3: {
+    {
       description: "item description 3",
       image_url: "https://picsum.photos/200",
       author: 1,
       category_id: 1,
     },
-    4: {
+    {
       description: "item description 4",
       image_url: "https://picsum.photos/200",
       author: 1,
       category_id: 1,
     },
-    5: {
+    {
       description: "item description 5",
       image_url: "https://picsum.photos/200",
       author: 1,
       category_id: 1,
     },
-    6: {
+    {
       description: "item description 6",
       image_url: "https://picsum.photos/200",
       author: 1,
       category_id: 1,
     },
-    7: {
+    {
       description: "item description 7",
       image_url: "https://picsum.photos/200",
       author: 2,
       category_id: 1,
     },
-    8: {
+    {
       description: "item description 8",
       image_url: "https://picsum.photos/200",
       author: 2,
       category_id: 1,
     },
-    9: {
+    {
       description: "item description 9",
       image_url: "https://picsum.photos/200",
       author: 2,
       category_id: 1,
     },
-    10: {
+    {
       description: "item description 10",
       image_url: "https://picsum.photos/200",
       author: 2,
       category_id: 1,
     },
-    11: {
+    {
       description: "item description 11",
       image_url: "https://picsum.photos/200",
       author: 2,
       category_id: 1,
     },
-    12: {
+    {
       description: "item description 12",
       image_url: "https://picsum.photos/200",
       author: 2,
       category_id: 1,
     },
-    13: {
+    {
       description: "item description 13",
       image_url: "https://picsum.photos/200",
       author: 2,
       category_id: 1,
     },
-    14: {
+    {
       description: "item description 14",
       image_url: "https://picsum.photos/200",
       author: 2,
       category_id: 1,
     },
-    15: {
+    {
       description: "item description 15",
       image_url: "https://picsum.photos/200",
       author: 1,
       category_id: 1,
     },
-    16: {
+    {
       description: "item description 16",
       image_url: "https://picsum.photos/200",
       author: 1,
       category_id: 1,
     },
-    17: {
+    {
       description: "item description 17",
       image_url: "https://picsum.photos/200",
       author: 1,
       category_id: 2,
     },
-    18: {
+    {
       description: "item description 18",
       image_url: "https://picsum.photos/200",
       author: 2,
       category_id: 2,
     },
-  },
+  ],
   totalItems: 18,
 };
 
