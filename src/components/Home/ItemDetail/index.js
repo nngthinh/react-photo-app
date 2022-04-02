a596d17e3c275365b491e512a60f4308456e7a5d
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-04-02 21:28:12 +0700

    refactor: remove unused import

--- src/components/Home/ItemDetail/index.js
+++ src/components/Home/ItemDetail/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { Skeleton } from "@ahaui/react";
